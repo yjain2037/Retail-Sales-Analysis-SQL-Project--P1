8344c751a07f5af48a908308b687f1072f2d9046
Yash Jain <yjain2037@gmail.com>
2026-03-12 14:52:43 +0530

    Refactor SQL queries for retail sales analysis

--- sql_project _1.sql
+++ sql_project _1.sql
@@ -174,22 +174,7 @@ count(distinct customer_id) as cnt_uniq_cs
 from retail_sales
 group by 1
 
---Q.10 Write a SQL query to create each shift and number of orders (Example Morning <12, Afternoon Between 12 & 17, Evening >17)
-WITH hourly_sale
-AS
-(
-SELECT *,
-    CASE
-        WHEN EXTRACT(HOUR FROM sale_time) < 12 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
-        ELSE 'Evening'
-    END as shift
-FROM retail_sales
-)
-SELECT 
-    shift,
-    COUNT(*) as total_orders    
-FROM hourly_sale
-GROUP BY shift
+
 
 ----end of project 
+
